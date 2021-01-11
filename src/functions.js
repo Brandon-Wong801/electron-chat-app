9290038f2df04c7a83543f1962927bb7c2823458
Brandon Wong <u0911623@umail.utah.edu>
2021-01-11 16:04:34 -0700

    added new UI and moving to each page, having trouble with using variables between the pages

--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,32 @@
-const enterButton = document.getElementById('enter');
+//const enterButton = document.getElementById('enter');
 
 
 
-enterButton.onclick = enterClicked;
+//enterButton.onclick = enterClicked;
 var count = 0;
 function enterClicked(){
     const name = document.getElementById('uname').value;
     console.log("Test worked");
     count++;
     //window.alert(name+" "+ count);
-    document.getElementById('username').innerText = name;
+    enterChat(name);
+    
+    
 }
 
+function homeClicked(){
+    window.location.href = "index.html";
+    populateHeader("hi");
+}
+
+function enterChat(name){
+    window.location.href = "chatroom.html";
+    window.onload = populateHeader(name);
+    window.alert(name);
+}
+
+function populateHeader(name){
+    document.getElementById('username').innerText = name;
+}
 
 
